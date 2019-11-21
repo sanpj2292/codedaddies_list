2b84ba59e98ba535374c24aa9496c77569d5ab4f
Sai Sankeerth <sankeerth2292@gmail.com>
2019-11-21 20:27:17 +0530

    change in staticfiles directory variable to load CSS

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'codedaddies_list.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
